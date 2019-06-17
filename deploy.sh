2dcf93b8d453b17e8d941f115fc888f55fdd6c7b
Muralidhar Hegde <hmuralid@in.ibm.com>
2019-06-17 16:04:34 +0530

    updated deploy.sh with echo statements

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,8 @@ npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 
 echo "Deploying to $env"
 echo "_______________________________"
+echo "env:$env"
+echo "CODEBUILD_SRC_DIR:$CODEBUILD_SRC_DIR"
+echo "_______________________________"
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
+echo "serverless deploy completed"
